8226b2c190cbf99086d3a564f858739ebb8b22b4
arun-357s <arunachalamnachiappan20@gmail.com>
2025-06-21 12:41:26 +0530

    fix:issue

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Elevator elevator1 = new Elevator(1);
-        Elevator elevator2 = new Elevator(2);
-        List<Elevator> elevators = new ArrayList<>();
-        elevators.add(elevator1);
-        elevators.add(elevator2); // or Arrays.asList(e1, e2)
-        ElevatorController controller = new ElevatorController(elevators);
+    public static void main(String[] args) throws InterruptedException {
+        Elevator e1 = new Elevator(1);
+        Elevator e2 = new Elevator(2);
+        ElevatorController controller = new ElevatorController(Arrays.asList(e1, e2));
 
         controller.handleRequest(new Request(Direction.UP, 5));
-        controller.handleRequest(new Request(Direction.DOWN, 3));
-        controller.handleRequest(new Request(Direction.UP, 7));
+        controller.processAllRequests(); Thread.sleep(1000);
 
-        for (int i= 0; i < 10; i++) {
+        controller.handleRequest(new Request(Direction.DOWN, 3));
+        controller.processAllRequests(); Thread.sleep(1000);
+
+        controller.handleRequest(new Request(Direction.DOWN, 1));
+        controller.processAllRequests(); Thread.sleep(1000);
+
+        controller.handleRequest(new Request(Direction.DOWN, 1));
+        controller.processAllRequests(); Thread.sleep(1000);
+
+        controller.handleRequest(new Request(Direction.DOWN, 0));
+        controller.processAllRequests(); Thread.sleep(1000);
+
+        controller.handleRequest(new Request(Direction.UP, 7));
+        for (int i = 0; i < 10; i++) {
             controller.processAllRequests();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignored) {}
+            Thread.sleep(1000);
         }
     }
 }
